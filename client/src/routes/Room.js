ac9a31441c7671b0b78660677da9e8bcddd9d1a0
fakhripraya <fakhri@indomaret.co.id>
2022-12-11 23:04:50 +0700

    Revert "sync server with client"
    This reverts commit 057980803ad63217641fada3fd8c14ed8d003e89.

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -8,7 +8,6 @@ const Room = (props) => {
     const socketRef = useRef();
     const otherUser = useRef();
     const userStream = useRef();
-    const senders = useRef([]);
 
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
@@ -38,9 +37,7 @@ const Room = (props) => {
 
     function callUser(userID) {
         peerRef.current = createPeer(userID);
-        userStream.current.getTracks().forEach((track) => {
-            senders.current.push(peerRef.current.addTrack(track, userStream.current));
-        });
+        userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
     }
 
     function createPeer(userID) {
@@ -65,7 +62,6 @@ const Room = (props) => {
     }
 
     function handleNegotiationNeededEvent(userID) {
-        console.log("handle negotiation")
         peerRef.current.createOffer().then(offer => {
             return peerRef.current.setLocalDescription(offer);
         }).then(() => {
@@ -123,22 +119,11 @@ const Room = (props) => {
         partnerVideo.current.srcObject = e.streams[0];
     };
 
-    function shareScreen() {
-        navigator.mediaDevices.getDisplayMedia({ cursor: true }).then(stream => {
-            const screenTrack = stream.getTracks()[0];
-            senders.current.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
-            screenTrack.onended = function () {
-                senders.current.find(sender => sender.track.kind === 'video').replaceTrack(userStream.current.getTracks()[1]);
-            }
-        })
-    }
-
     return (
         <div>
-            <video controls autoPlay ref={userVideo} />
-            <video controls autoPlay ref={partnerVideo} />
-            <button onClick={() => { shareScreen() }}></button>
-        </div >
+            <video autoPlay ref={userVideo} />
+            <video autoPlay ref={partnerVideo} />
+        </div>
     );
 };
 
